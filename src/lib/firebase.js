ca41841e5420b6d6ae28753b3397052f6b09d47e
Rohan Patel <rohanpatel1531@gmail.com>
2022-02-12 17:59:06 +0530

    ...

---
+++ src/lib/firebase.js
@@ -0,0 +1,26 @@
+import Firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+
+// here I want to import the seed file
+// import { seedDatabase } from '../seed'
+
+const config = {
+  apiKey: 'AIzaSyBIhS5PIpIEcoyoY-uUQAJA35qIgzUGkEM',
+  authDomain: 'instagram-clone-a8e53.firebaseapp.com',
+  projectId: 'instagram-clone-a8e53',
+  storageBucket: 'instagram-clone-a8e53.appspot.com',
+  messagingSenderId: '526758550860',
+  appId: '1:526758550860:web:b024e2f720cf72b27b83e9',
+  measurementId: 'G-GY1H57F99X',
+}
+
+const firebase = Firebase.initializeApp(config)
+const { FieldValue } = Firebase.firestore
+
+console.log('firebase', firebase)
+
+// here is where I want to call seed file (ONLY once)
+// seedDatabase(firebase)
+
+export { firebase, FieldValue }
--- src/seed.js
+++ src/seed.js
@@ -1,7 +1,7 @@
 export function seedDatabase(firebase) {
   const users = [
     {
-      userId: 'NvPY9M9MzFTARQ6M816YAzDJxZ72',
+      userId: '1sfeM3zj53OKwCkCGqxLWJ3W1Bi1',
       username: 'karl',
       fullName: 'Karl Hadwen',
       emailAddress: 'karlhadwen@gmail.com',
@@ -15,7 +15,7 @@ export function seedDatabase(firebase) {
       fullName: 'Raffaello Sanzio da Urbino',
       emailAddress: 'raphael@sanzio.com',
       following: [],
-      followers: ['NvPY9M9MzFTARQ6M816YAzDJxZ72'],
+      followers: ['1sfeM3zj53OKwCkCGqxLWJ3W1Bi1'],
       dateCreated: Date.now(),
     },
     {
@@ -24,7 +24,7 @@ export function seedDatabase(firebase) {
       fullName: 'Salvador Dalí',
       emailAddress: 'salvador@dali.com',
       following: [],
-      followers: ['NvPY9M9MzFTARQ6M816YAzDJxZ72'],
+      followers: ['1sfeM3zj53OKwCkCGqxLWJ3W1Bi1'],
       dateCreated: Date.now(),
     },
     {
@@ -33,7 +33,7 @@ export function seedDatabase(firebase) {
       fullName: 'George Orwell',
       emailAddress: 'george@orwell.com',
       following: [],
-      followers: ['NvPY9M9MzFTARQ6M816YAzDJxZ72'],
+      followers: ['1sfeM3zj53OKwCkCGqxLWJ3W1Bi1'],
       dateCreated: Date.now(),
     },
   ]
