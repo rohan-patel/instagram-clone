8fe29d4f397e695570ac6b171d32e5f71fbd738f
Rohan Patel <rohanpatel1531@gmail.com>
2022-02-13 11:45:57 +0530

    ...

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
 import { useContext, useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import FirebaseContext from '../context/firebase'
+import image from '../../public/images/iphone-with-profile.jpg'
 
 export default function Login() {
   const history = useNavigate()
@@ -18,7 +19,14 @@ export default function Login() {
     document.title = 'Login - Instagram'
   }, [])
 
-  return <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
-    <p>This a Login Page</p>
-  </div>
+  return (
+    <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
+      <div className='flex w-3/5'>
+        <img
+          src={image}
+          alt='Iphone with Instagram'
+        />
+      </div>
+    </div>
+  )
 }
